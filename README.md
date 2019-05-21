d71ca54b2d705da59e343273dac1ebd3e92c6ee1
coolreader18 <33094578+coolreader18@users.noreply.github.com>
2019-05-21 17:40:06 -0500

    Prep for release

--- README.md
+++ README.md
@@ -2,6 +2,17 @@
 
 > A Lisp dialect designed for codingworkshops
 
+## Example
+
+```lisp
+(def fib (n)
+    (if (<= n 2) 1
+        (-
+            (fib (- n 1))
+            (fib (- n 2)))))
+(var fib10 (fib 10))
+```
+
 ## Running examples
 
 ```sh
@@ -11,7 +22,21 @@ elm reactor
 
 Navigate to http://localhost:8000/Main.elm
 
+## Usage
+
+```elm
+import Lisa
+
+doParse source =
+    case Lisa.processString source of
+        Ok program ->
+            -- do something with program
+
+        Err err ->
+          Err err
+```
+
 ## License
 
-This project is licensed under the GPL v3. See the [LICENSE](LICENSE) file
-for more details.
+This project is licensed under the GPL v3. See the [LICENSE](LICENSE) file for
+more details.
--- src/Lisa.elm
+++ src/Lisa.elm
@@ -1,17 +1,41 @@
 module Lisa exposing (processString, processStringToJson)
 
+{-|
+
+    # Lisa
+
+    A simple lisp
+    Example:
+    ```
+    (def fib (n)
+        (if (<= n 2) 1
+            (-
+                (fib (- n 1))
+                (fib (- n 2)))))
+    (var fib10 (fib 10))
+    ```
+
+    @docs processString
+    @docs processStringToJson
+
+-}
+
 import Common exposing (Error)
 import Json.Encode as E
 import Lisa.Parser
 import Lisa.Process
 
 
+{-| Parse a string into a program
+-}
 processString : String -> Result Error Lisa.Process.Program
 processString input =
     Lisa.Parser.parse input
         |> Result.andThen Lisa.Process.processProgram
 
 
+{-| Parse a string into a Json representation of a program
+-}
 processStringToJson : String -> Result E.Value E.Value
 processStringToJson input =
     processString input
