76b1d86266e5b8630419dd5991220b279a2dce2f
coolreader18 <33094578+coolreader18@users.noreply.github.com>
2019-05-24 21:47:49 -0500

    Update examples again

--- README.md
+++ README.md
@@ -5,7 +5,7 @@
 ## Example
 
 ```lisp
-(defunc fib (n)
+(defunc fib-recurse (n)
     (var a 0) (var b 1) (var f 1)
     (defunc calc-fib (i)
         (if (<= i n)
@@ -16,7 +16,16 @@
                 (calc-fib (+ i 1)))
             f))
     (calc-fib 2))
-(= (fib 10) 55)
+(defunc fib-while (n)
+    (var a 0) (var b 1) (var f 1)
+    (var i 2)
+    (while (<= i n)
+        (set f (+ a b))
+        (set a b)
+        (set b f)
+        (set i (+ i 1))
+        f))
+(= (fib-recurse 10) (fib-while 10) 55)
 ```
 
 ## Running examples
--- src/Lisa.elm
+++ src/Lisa.elm
@@ -12,7 +12,7 @@ module Lisa exposing
     A simple lisp
     Example:
     ```
-    (defunc fib (n)
+    (defunc fib-recurse (n)
         (var a 0) (var b 1) (var f 1)
         (defunc calc-fib (i)
             (if (<= i n)
@@ -23,7 +23,16 @@ module Lisa exposing
                     (calc-fib (+ i 1)))
                 f))
         (calc-fib 2))
-    (= (fib 10) 55)
+    (defunc fib-while (n)
+        (var a 0) (var b 1) (var f 1)
+        (var i 2)
+        (while (<= i n)
+            (set f (+ a b))
+            (set a b)
+            (set b f)
+            (set i (+ i 1))
+            f))
+    (= (fib-recurse 10) (fib-while 10) 55)
     ```
 
     @docs parseProgram
